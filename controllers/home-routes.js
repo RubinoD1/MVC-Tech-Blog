df576854128b600ed7424512239512e01954b92f
RubinoD1 <Rubinod1@gmail.com>
2022-08-09 07:12:07 -0400

    posts populating homepage, base homepage.handlebars template working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,17 +1,40 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  //specifies what template we want to use
-  res.render('homepage', {
-    id: 1,
-    post_content: 'Hello world',
-    title: 'Title of my blog post',
-    created_at: new Date(),
-    comments: [{}, {}],
-    user: {
-      username: 'test_user'
-    }
-  });
+  Post.findAll({
+    attributes: [
+      'id',
+      'post_content',
+      'title',
+      'created_at'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
+    ]
+  })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      // pass a single post object into the homepage template
+      
+      res.render('homepage', { posts });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;

cba6c964121bde310d541c6a8d3a3912809d5252
RubinoD1 <Rubinod1@gmail.com>
2022-08-09 09:29:13 -0400

    express-session and connect-session added: login working

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -58,11 +58,15 @@ router.post('/', (req, res) => {
       username: req.body.username,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
       });
+    })
   });
 
   router.post('/login', (req, res) => {
@@ -78,14 +82,27 @@ router.post('/', (req, res) => {
         }
         // Verify user
         const validPassword = dbUserData.checkPassword(req.body.password);
+
         if (!validPassword) {
           res.status(400).json({ message: 'Incorrect password!' });
           return;
         }
-        
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-      });  
+    
+        req.session.save(() => {
+          // declare session variables
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
+      });
     });
+
+//logout route 
+router.post('/logout', (req, res) => {
+
+});
 
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,6 +3,8 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log(req.session);
+  
   Post.findAll({
     attributes: [
       'id',
@@ -27,14 +29,22 @@ router.get('/', (req, res) => {
   })
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
-      // pass a single post object into the homepage template
-      
       res.render('homepage', { posts });
     })
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+//check for a session and redirect to the homepage if one exists
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
